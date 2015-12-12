4357f08743dc3fc0c89d87753b45d0c00edb739b
alaija <alaija@ya.ru>
2015-12-12 17:28:42 +0300

    Initial Commit

---
+++ RaceData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RaceData
+//
+//  Created by Alan Kochev on 12/12/15.
+//  Copyright © 2015 Alan Kochev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RaceData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RaceData
+//
+//  Created by Alan Kochev on 12/12/15.
+//  Copyright © 2015 Alan Kochev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
